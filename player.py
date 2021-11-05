2c07ee62ab9dc97d23655f127548e43fe5ddcdb7
Christian Schuster <cschuste@synopsys.com>
2021-11-05 13:45:30 +0100

    1st dummy check-in

--- player.py
+++ player.py
@@ -3,7 +3,13 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 0
+        current_buy_in = game_state["current_buy_in"]
+        in_action = game_state["in_action"]
+        players = game_state["players"]
+
+        # just check everything
+        check_value = current_buy_in - players[in_action]["bet"]
+        return check_value
 
     def showdown(self, game_state):
         pass
